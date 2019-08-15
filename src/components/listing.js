229be9e817ac83da457e470434cceb565d07e468
BrainJarProductions <vgrilli1@gmail.com>
2019-08-15 19:40:21 -0400

    refactor index to use Listing component

--- src/components/listing.js
+++ src/components/listing.js
@@ -1,16 +1,41 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 import Image from '../components/image';
-const Listing = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-);
+const Listing = () => {
+  const LISTING_QUERY = useStaticQuery(graphql`
+    query BlogPostListing {
+      allMarkdownRemark(
+        limit: 5
+        sort: { order: DESC, fields: [frontmatter___date] }
+      ) {
+        edges {
+          node {
+            excerpt
+            frontmatter {
+              date
+              title
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <>
+      {LISTING_QUERY.allMarkdownRemark.edges.map(({ node }) => (
+        <article key={node.frontmatter.slug}>
+          <Link to={`/posts${node.frontmatter.slug}`}>
+            <h2>{node.frontmatter.title}</h2>
+          </Link>
+          <p>{node.frontmatter.date}</p>
+          <p>{node.excerpt}</p>
+          <Link to={`/posts${node.frontmatter.slug}`}>Read More</Link>
+        </article>
+      ))}
+    </>
+  );
+};
 
 export default Listing;
