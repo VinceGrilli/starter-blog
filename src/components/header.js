d8f3fd4936126f6156e38628addfc77fb57ac2cd
BrainJarProductions <vgrilli1@gmail.com>
2019-08-15 21:04:17 -0400

    add some styling

--- src/components/header.js
+++ src/components/header.js
@@ -13,7 +13,7 @@ const HeaderWrapper = styled.div`
 const HeaderContainer = styled.div`
   margin: 0 auto;
   max-width: 960px;
-  padding: 1rem;
+  padding: 0.5rem;
   img {
     margin-bottom: 0;
   }
--- src/components/listing.js
+++ src/components/listing.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
 import Image from '../components/image';
+import styled from 'styled-components';
+
+const Post = styled.article`
+  box-shadow: 0px 3px 10px rgba(25, 17, 34, 0.05);
+  padding: 1rem;
+  border-radius: 4px;
+  margin-bottom: 1rem;
+  a {
+    color: #000;
+    text-decoration: none;
+  }
+  h2 {
+    margin-bottom: 0;
+  }
+  p {
+    font-size: 0.8rem;
+  }
+  .read-more {
+    font-size: 0.8rem;
+    text-decoration: underline;
+    color: #524763;
+  }
+`;
+
 const Listing = () => {
   const LISTING_QUERY = useStaticQuery(graphql`
     query BlogPostListing {
@@ -25,14 +49,16 @@ const Listing = () => {
   return (
     <>
       {LISTING_QUERY.allMarkdownRemark.edges.map(({ node }) => (
-        <article key={node.frontmatter.slug}>
+        <Post key={node.frontmatter.slug}>
           <Link to={`/posts${node.frontmatter.slug}`}>
             <h2>{node.frontmatter.title}</h2>
           </Link>
           <p>{node.frontmatter.date}</p>
           <p>{node.excerpt}</p>
-          <Link to={`/posts${node.frontmatter.slug}`}>Read More</Link>
-        </article>
+          <Link class="read-more" to={`/posts${node.frontmatter.slug}`}>
+            Read More
+          </Link>
+        </Post>
       ))}
     </>
   );
